6b6bc3b985deee59ddb1b8e7731c938abf8a4172
Paul Solt <PaulSolt@gmail.com>
2020-04-17 14:29:50 -0400

    Added starter project and image

---
+++ iOS14-TL-StickyHeader/iOS14-TL-StickyHeader/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  iOS14-TL-StickyHeader
+//
+//  Created by Paul Solt on 4/17/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+}
